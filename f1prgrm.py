33a2b764005a9452c83e82db631ee0f7add3b7d3
Subha-46 <153758784+Subha-46@users.noreply.github.com>
2024-05-15 22:05:36 +0530

    Update calculator.py

--- f1prgrm.py
+++ f1prgrm.py
@@ -1,8 +1,54 @@
-# printing
-print("hello")
-print("5")
-print("bye") 
-print(5*5) 
-print("Hey i am \"Subhajit Das\"\ni am 18")
-print("hello", 6,7, sep="--" , end="46\n")
-print("hi") 
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # take input from the user
+    choice = input("Enter choice (1/2/3/4): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first digit  number: "))
+            num2 = float(input("Enter second digit number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        
+        # check if user wants another calculation
+        
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    else:
+        print("Invalid Input")
+
+
